cffee2e1fb456b34f98ec4102b2b4af1f255d8a7
Yanfei.Luo <491428731@qq.com>
2021-07-05 20:23:39 +0800

    添加 xIsNext，实现轮流落子

--- src/index.js
+++ src/index.js
@@ -2,31 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//     // constructor(props){
-//     //     super(props) //固定的开头
-//     //     this.state = {
-//     //         value: null
-//     //     }
-//     // }
-
-//     render() {
-//         return (
-//             <button 
-//                 className="square"
-//                 onClick={
-//                     () => {this.props.onClick() }
-//                 }
-//             >
-//                 {this.props.value}
-//             </button>
-//         );
-//     }
-// }
-
-//只包含一个redner方法、没有state的组件，可以使用函数组件。
-//以下改写 Square 组件：
-
 function Square(props){
     return(
         <button className="square" onClick={props.onClick}>
@@ -34,29 +9,23 @@ function Square(props){
         </button>
     )
 }
-
-
 class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
 
     handleClick(i){
-        const squares = this.state.squares.slice(); //拷贝了一个数组
-        squares[i] = 'X' //修改新数组，而不是原数组
+        const squares = this.state.squares.slice(); 
+        squares[i] = this.state.xIsNext?'X':'O'
         this.setState({
-            squares:squares  //赋值新数组
+            squares:squares, 
+            xIsNext: !this.state.xIsNext
         })
     }
-
-    //不直接修改原数据，方便实现撤销和恢复功能。 —— 那如果我不需要这个功能？
-    //虽然UIDP确实有这个需求。
-
-    //作用：可以轻松确定数据是否发生了改变，从而确定组件是否需要重新渲染 —— 我想起UIDP中那个，需要 watch+computed 配合判断是否改变数据的场景
-    //这是性能优化的一个方向 —— 配合 shouldComponentUpdate 使用
 
     renderSquare(i) {
         return (
@@ -64,17 +33,16 @@ class Board extends React.Component {
                 value={this.state.squares[i]}
                 onClick={()=>{this.handleClick(i)}}
             />
-        ) //return 的内容多行，需要用小括号包裹
+        )
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');;
 
         return (
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {/* Question:为什么不直接写 <Square /> 呢？*/}
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
